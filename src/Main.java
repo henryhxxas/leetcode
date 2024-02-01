05c6024a6a2cfe796db578c3c66a247e8e452d8c
henryhxxas <731085203@qq.com>
2024-02-01 15:11:27 +0800

    测试提交

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: hhx
+ * @Date: 2024/2/1 11:07
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
