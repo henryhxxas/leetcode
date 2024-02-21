d06df730349779758b97444761384ed7ddf99981
henryhxxas <731085203@qq.com>
2024-02-21 17:02:28 +0800

    leetcode49字母异位词分组

--- src/code2_Group_Anagrams.java
+++ src/code2_Group_Anagrams.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 /**
  * @Author: hhx
  * @Date: 2024/2/20 15:08
@@ -5,4 +7,23 @@
  * @Version: 1.0
  */
 public class code2_Group_Anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str:strs){
+            char[] chars=str.toCharArray();
+            Arrays.sort(chars);
+            //System.out.println(new String(chars));
+            String key=new String(chars);
+            List<String> list=map.getOrDefault(key,new ArrayList<String>());
+            list.add(str);
+            map.put(key,list);
+
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+
+    public static void main(String[] args) {
+        code2_Group_Anagrams groupAnagrams=new code2_Group_Anagrams();
+        groupAnagrams.groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"});
+    }
 }
