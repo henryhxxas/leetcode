7a12b8cec62e0da19f3c1ab2c0b720463ac813f4
henryhxxas <731085203@qq.com>
2024-02-27 10:37:24 +0800

    leetcode3. 无重复字符的最长子串

--- src/code8_Longest_Substring_Without_Repeating_Characters.java
+++ src/code8_Longest_Substring_Without_Repeating_Characters.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * @Author: hhx
  * @Date: 2024/2/27 10:34
@@ -5,4 +7,20 @@
  * @Version: 1.0
  */
 public class code8_Longest_Substring_Without_Repeating_Characters {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0){
+            return 0;
+        }
+        HashMap<Character,Integer> map=new HashMap<Character,Integer>();
+        int max=0;
+        int left=0;
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                left=Math.max(left,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max=Math.max(max,i-left+1);
+        }
+        return max;
+    }
 }
