b322bfeefbd6676e2787d5b3bd6249147c2d2e0e
henryhxxas <731085203@qq.com>
2024-03-15 10:26:34 +0800

    leetcode48. 旋转图像

---
+++ src/code19_Rotate_Image.java
@@ -0,0 +1,27 @@
+/**
+ * @Author: hhx
+ * @Date: 2024/3/15 10:26
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class code19_Rotate_Image {
+    //先对角线互换位置，再竖着对半翻转
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int j=0;j<m/2;j++){
+            for(int i=0;i<n;i++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][m-1-j];
+                matrix[i][m-1-j]=temp;
+            }
+        }
+    }
+}
