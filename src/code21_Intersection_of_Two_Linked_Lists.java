710f484b0f67c25857b6d3d5f50c58099ca86bff
henryhxxas <731085203@qq.com>
2024-03-19 15:12:36 +0800

    leetcode160. 相交链表

---
+++ src/code21_Intersection_of_Two_Linked_Lists.java
@@ -0,0 +1,30 @@
+/**
+ * @Author: hhx
+ * @Date: 2024/3/19 11:14
+ * @Description: TODO
+ * @Version: 1.0
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class code21_Intersection_of_Two_Linked_Lists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA==null||headB==null) return null;
+        ListNode pA=headA,pB=headB;
+        while(pA!=pB){
+            pA=pA==null?headB:pA.next;
+            pB=pB==null?headA:pB.next;
+        }
+        return pA;
+    }
+
+}
