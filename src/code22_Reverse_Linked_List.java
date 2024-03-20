e8a26d65de683ff03f1c34f329cbd36536bb6ecc
henryhxxas <731085203@qq.com>
2024-03-20 19:28:23 +0800

    leetcode206. 反转链表

---
+++ src/code22_Reverse_Linked_List.java
@@ -0,0 +1,27 @@
+/**
+ * @Author: hhx
+ * @Date: 2024/3/20 19:27
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class code22_Reverse_Linked_List {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur=head;
+        ListNode pre=null;
+        while(cur!=null){
+            ListNode tmp=cur.next;
+            cur.next=pre;
+            pre=cur;
+            cur=tmp;
+
+        }
+        return pre;
+    }
+     public class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
